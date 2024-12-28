26fe519e38bcaf75f29ca72c1c60fbc9cc60ec59
Sanket Kumar Padhan <152056082+Sanket3yoProgrammer@users.noreply.github.com>
2024-12-28 21:00:56 +0530

    Delete ai_chat.py

--- ai_chat.py
+++
@@ -1,6 +0,0 @@
-from webscout import PhindSearch
-
-def bot_response(user_text):
-    ai = PhindSearch(quiet=True, is_conversation=None)
-    res = ai.chat(user_text)  # internal stream is not available for this Provider
-    return res
